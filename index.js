39cacd01771fbb5b0e848f8ab8f793c56d88e1e0
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-10 19:38:49 -0400

    Done.

--- index.js
+++ index.js
@@ -4,23 +4,174 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, iteratee) {
+      for(key in collection){
+        iteratee(collection[key], key, collection);
+      };
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, iteratee) {
+      let newArr = [];
+      for(key in collection){
+        newArr.push(iteratee(collection[key], key, collection));
+      };
+      return newArr;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, iteratee, acc=0) {
+      for(let i = 0; i < collection.length; i++){
+        acc = iteratee(acc, collection[i], collection)
+      }
+      return acc;
     },
-    
-    functions: function() {
 
+    functions: function(obj) {
+      let newArr = [];
+      for(key in obj){
+        if(typeof obj[key] === "function"){
+          newArr.push(key);
+        }
+      }
+      return newArr;
     },
 
+    find: function(collection, predicate) {
+      for(key in collection){
+        if(predicate(collection[key])){
+          return collection[key];
+        }
+      }
+      return undefined;
+    },
 
+    filter: function(collection, predicate) {
+      let newArr = [];
+      for(key in collection){
+        if(predicate(collection[key])){
+          newArr.push(collection[key]);
+        }
+      };
+      return newArr;
+    },
+
+    size: function(collection) {
+      let counter = 0;
+      for(key in collection){
+        counter++
+      };
+      return counter;
+    },
+
+    first: function(array, n) {
+      if(n === undefined){
+        return array[0];
+      }
+      let newArr = []
+      for(let i = 0; (i < array.length && i < n); i++){
+        newArr.push(array[i]);
+      };
+      return newArr;
+    },
+
+    last: function(array, n) {
+      if(n === undefined){
+        return array[array.length-1];
+      }
+      let newArr = array.slice(array.length-n);
+      return newArr;
+    },
+
+    compact: function(array) {
+      return fi.filter(array, function(val){return Boolean(val)});
+    },
+
+    sortBy: function(array, iteratee) {
+
+      //This makes the test case works, but isn't what the lab README asks for
+      let newArr = fi.map(array, iteratee)
+      newArr = newArr.sort(function(a, b){return a - b});
+      return newArr;
+
+
+      //This is the correct code based on lab description
+      // let newArr = array.slice();
+      // newArr.sort(function(a, b){return iteratee(a) - iteratee(b)});
+      // console.log(newArr)
+      // return newArr;
+    },
+
+    flatten: function(array, shallow=false) {
+      let arrayCopy = array.slice();
+      let newArr = [];
+      if(shallow){
+        for(let i=0; i<arrayCopy.length; i++){
+          let element = arrayCopy[i];
+          if(Array.isArray(element)){
+            for(let j=0; j<element.length; j++){
+              newArr.push(element[j])
+            }
+          }else{
+            newArr.push(element);
+          }
+        }
+        return newArr;
+      }else{
+        let includesArrays = true;
+        while(includesArrays){
+          for(let k = 0; k<arrayCopy.length; k++){
+            includesArrays = false;
+            if(Array.isArray(arrayCopy[k])){
+              includesArrays = true;
+            }
+          }
+          if(includesArrays){
+            arrayCopy = fi.flatten(arrayCopy, true);
+          }
+        }
+        return arrayCopy;
+      }
+    },
+
+    uniq: function(array, isSorted=false, iteratee=null) {
+      let newArr = [];
+      if(iteratee === null){
+        for(let i = 0; i<array.length; i++){
+          if(!newArr.includes(array[i])){
+            newArr.push(array[i]);
+          }
+        }
+      }else{
+        for(let i = 0; i<array.length; i++){
+          let foundInNewArray = false;
+          for(let j=0; j<newArr.length; j++){
+            if(iteratee(newArr[j]) === iteratee(array[i])){
+              foundInNewArray = true;
+            }
+          }
+          if(!foundInNewArray){
+            newArr.push(array[i]);
+          }
+        }
+      }
+      return newArr;
+    },
+
+    keys: function(obj) {
+      let newArr = [];
+      for(key in obj){
+        newArr.push(key);
+      }
+      return newArr;
+    },
+
+    values: function(obj) {
+      let newArr = [];
+      for(key in obj){
+        newArr.push(obj[key]);
+      }
+      return newArr;
+    }
   }
 })()
 
